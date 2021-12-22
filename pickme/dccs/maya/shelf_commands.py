757b6f2f92930a91f343f7682d1217625e63025f
PiloeGAO <leonumerique@gmail.com>
2021-12-22 13:37:58 +0100

    ADD: Support for opening PickMe inside of the Maya UI as custom Tab

--- pickme/dccs/maya/shelf_commands.py
+++ pickme/dccs/maya/shelf_commands.py
@@ -7,11 +7,14 @@
 '''
 import webbrowser
 
+from pickme.widgets.main_widget import MainWidget
+from pickme.dccs.maya.maya_widgets import DockableWidgetUIScript
 
 def open_ui():
     """Open the interface of PickMe.
     """
-    pass
+    pickme_main_widget = MainWidget()
+    DockableWidgetUIScript(window_name="PickMe", widget=pickme_main_widget, restore=False)
 
 def open_doc():
     """Open the project documentation on the web browser.
