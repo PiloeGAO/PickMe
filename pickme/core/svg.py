7ea0b521b5fc2f9cd2b42a5a12c1bde1ac900e49
PiloeGAO <leonumerique@gmail.com>
2022-01-07 15:50:03 +0100

    ADD: Improving the SVG inheritance

--- pickme/core/svg.py
+++ pickme/core/svg.py
@@ -9,8 +9,14 @@ import os
 import xml.etree.ElementTree as ET
 
 class SVG(object):
-    def __init__(self, id="") -> None:
+    def __init__(self, id="", parent=None, childs=[]) -> None:
         self.id = id
+        self.parent = parent
+        self.childs = childs
+
+    def add_child(self, child):
+        child.parent = self
+        self.childs.append(child)
 
 class SVGDocument(SVG):
     def __init__(self, path="") -> None:
@@ -19,11 +25,9 @@ class SVGDocument(SVG):
         self.height = 100
         self.version = 1.1
 
-        self.layers = []
-
         if(os.path.isfile(path)):
             self.load_from_path(path)
-    
+
     def load_from_path(self, path):
         tree = ET.parse(path)
         root = tree.getroot()
@@ -42,50 +46,49 @@ class SVGDocument(SVG):
             self.width = int(viewBox[2])
             self.height = int(viewBox[3])
         
-        for layer in list(root):
-            if(layer.tag != "{http://www.w3.org/2000/svg}g"):
-                print(f"Element not supported: {layer.tag}")
-                continue
-            
-            layer_paths = []
-
-            for child in list(layer):
-                if(child.tag != "{http://www.w3.org/2000/svg}path"):
-                    print(f"Element not supported: {layer.tag}")
-                    continue
-
-                layer_paths.append(
-                    SVGPath(
-                        id=child.attrib["id"],
-                        raw_style=child.attrib["style"],
-                        raw_d=child.attrib["d"],
-                        raw_title=child["title"] if child.get("title", None) != None else "",
-                        raw_description=child["desc"] if child.get("desc", None) != None else ""
+        def recursive_import(parent, parent_class):
+            for elem in list(parent):
+                if(elem.tag == "{http://www.w3.org/2000/svg}g"):
+                    # Create layer
+                    new_layer = SVGLayer(
+                        id=elem.attrib["id"],
+                        childs=[]
                     )
-                )
+                    parent_class.add_child(
+                        new_layer
+                    )
+                    recursive_import(elem, new_layer)
 
-            self.layers.append(
-                SVGLayer(
-                    id=layer.attrib["id"],
-                    paths=layer_paths
-                )
-            )
+                elif(elem.tag == "{http://www.w3.org/2000/svg}path"):
+                    # Create path
+                    parent_class.add_child(
+                        SVGPath(
+                            id=elem.attrib["id"],
+                            raw_style=elem.attrib["style"],
+                            raw_d=elem.attrib["d"],
+                            raw_title=elem["title"] if elem.get("title", None) != None else "",
+                            raw_description=elem["desc"] if elem.get("desc", None) != None else ""
+                        )
+                    )
+                else:
+                    print("Skipping {elem.tag} (not supported).")
+
+        recursive_import(root, self)
 
 class SVGLayer(SVG):
-    def __init__(self, id="", paths=[]) -> None:
-        super(SVGLayer, self).__init__(id=id)
-        self.paths = paths
-
+    def __init__(self, *args, **kwargs) -> None:
+        super(SVGLayer, self).__init__(*args, **kwargs)
+    
 class SVGPath(SVG):
-    def __init__(self, id, raw_style, raw_d, raw_title, raw_description) -> None:
-        super(SVGPath, self).__init__(id=id)
+    def __init__(self, raw_style, raw_d, raw_title, raw_description, *args, **kwargs) -> None:
+        super(SVGPath, self).__init__(*args, **kwargs)
         self.raw_style = raw_style
         self.raw_d = raw_d
         self.raw_title = raw_title
         self.raw_description = raw_description 
 
 if(__name__ == "__main__"):
-    # TODO: Remove this whent the implementation is finished.
+    # TODO: Remove this when the implementation is finished.
     ROOT_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     GLOBAL_CONFIG_DIR = os.environ.get("pickme_configs", os.path.join(ROOT_DIR, "configs"))
     
